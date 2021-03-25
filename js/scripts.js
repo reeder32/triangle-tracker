6a8111be719dd0c4bc72fa2168718b80a4c2ba73
Nicholas Reeder <nickreeder32@gmail.com>
2021-03-25 15:26:46 -0700

    user can submit sides and return type of triangle will get error popup if sides are invalid
    Seth Medeiros <Medeirosseth@gmail.com>
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    event.preventDefault();
+
+    const side1 = parseInt($("input#side1").val());
+    const side2 = parseInt($("input#side2").val());
+    const side3 = parseInt($("input#side3").val());
+    if (side1 > 0 && side2 > 0 && side3 > 0) {
+      if (side1 === side2 && side1 === side3) {
+        $("#type").text("Equilateral");
+      } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+        $("#type").text("Isosceles");
+      } else {
+        $("#type").text("Scalene");
+      }
+      $(".result").fadeIn();
+    } else {
+      $(".result").fadeOut();
+      alert("Incorrect input ;-(");
+    }
+  });
+});
