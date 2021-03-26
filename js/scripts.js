87eeaadd811ed4a0c348ce4b0fcc413a4d751003
Nicholas Reeder <nickreeder32@gmail.com>
2021-03-25 17:04:02 -0700

    add background color to output
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com
    Co-authored-by: Seth Medeiros <Medeirosseth@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
   $("#formOne").submit(function (event) {
-    event.preventDefault();
 
     const side1 = parseInt($("input#side1").val());
     const side2 = parseInt($("input#side2").val());
     const side3 = parseInt($("input#side3").val());
+
     if (side1 > 0 && side2 > 0 && side3 > 0) {
       if (side1 === side2 && side1 === side3) {
         $("#type").text("Equilateral");
@@ -18,5 +18,6 @@ $(document).ready(function () {
       $(".result").fadeOut();
       alert("Incorrect input ;-(");
     }
+    event.preventDefault();
   });
 });
